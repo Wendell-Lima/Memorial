14476efcc24ca51ba3b6f2f89110639c9b20e5ef
Wendell-Lima <wendelllima00@gmail.com>
2023-02-05 15:21:16 -0300

    Criando função dificuldade
    soframos

--- memoria.cpp
+++ memoria.cpp
@@ -19,34 +19,50 @@ void imprimirMatriz(char matriz[m][m]) {
 }
 */
 
-int espacosVazios(char matriz[m][m]) {
-	int cont=0, i, j;
-	for (i=0; i<m; i++)
-		for (j=0; j<m; j++) 
-			if (matriz[i][j] == ' ')
-				cont++;
-	if (cont != 0)
-		return cont+1;
-	return cont;
-}
+//Protótipos
+int espacosVazios(char [][m]);
+void imprimeTabuleiro(char [][m]);
+void jogar();
+void dificuldade();
 
-void imprimeTabuleiro(char matriz[m][m]) {
-	int i, j;
-	printf("  | ");
-	for (i=0; i<m; i++)
-		printf("%d  ", i);
-	printf("\n");
-	printf("--+-");
-	for (i=0; i<m; i++)
-		printf("---", i);
-	printf("\n");
-	for (i=0; i<m; i++) {
-		printf("%d | ", i);
-		for (j=0; j<m; j++) {
-			printf("%c  ", matriz[i][j]);
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	int continuar = 0, escolha;
+	
+	do{
+		system("cls");
+		printf("Seja bem vindo ao jogo da memória!\n");
+		printf("\n 1 - Jogar");
+		printf("\n 2 - Placar");
+		printf("\n 0 - Sair");
+		
+		printf("\n\nDigite a opção que desejar: ");
+		escolha = getche();
+		escolha -= 48;
+		fflush(stdin);
+		
+		switch (escolha) {
+			case 1:
+				jogar();
+				break;
+			/*case 2:
+				ranking();
+				break*/
+			case 0: // Sair / Fechar
+				printf("\nFechando...\n");
+				Sleep(500);
+				continuar = 1;
+				break;
+			default:
+				printf("Opção inválida!\n");
+				Sleep(500);
+				break;
 		}
-		printf("\n");
-	}
+	}while(!continuar);
+
+	system("pause");
+	return 0;
 }
 
 void jogar(){
@@ -60,6 +76,9 @@ void jogar(){
 	int linha[2], coluna[2];
 	int invalido=0;
 	int score = 0;
+	
+	//Pergunta ao usuário a dificuldade
+	dificuldade();
 	
 	// Inicializando cartas
 	for (i=0; i<numCartas; i++)
@@ -143,44 +162,77 @@ void jogar(){
 	} while (linha[i]+coluna[i]!=6);
 }
 
-
-int main() {
-	setlocale(LC_ALL, "Portuguese");
+void dificuldade(){
+	int continuar = 0;
+	int dificuldade;
 	
-	int continuar = 0, escolha;
 	do{
-		system("cls");
-		printf("Seja bem vindo ao jogo da memória!\n");
-		printf("\n 1 - Jogar");
-		printf("\n 2 - Placar");
-		printf("\n 0 - Sair");
+		printf("\n 0 - Voltar ao Menu");
+		printf("\n 1 - Fácil");
+		printf("\n 2 - Médio");
+		printf("\n 3 - Difícil");
+		printf("\n\nEscolha a dificuldade: ");
+		scanf("%d",&dificuldade);
 		
-		printf("\n\nDigite a opçãoo que desejar: ");
-		escolha = getche();
-		escolha -= 48;
+		dificuldade = getche();
+		dificuldade -= 48;
 		fflush(stdin);
 		
-		switch (escolha) {
-			case 1:
-				jogar();
+		switch(dificuldade){
+			case 0:
+				main();
 				break;
-			/*case 2:
-				ranking();
-				break*/
-			case 0: // Sair / Fechar
-				printf("Fechando...\n");
-				Sleep(500);
+			case 1:
+				#define m 4
+				continuar = 1;
+				break;
+			case 2:
+				#define m 5
+				continuar = 1;
+				break;
+			case 3:
+				#define m 6
 				continuar = 1;
 				break;
 			default:
 				printf("Opção inválida!\n");
 				Sleep(500);
 				break;
-		}
+			
+		}	
 	}while(!continuar);
-
-	system("pause");
-	return 0;
 }
+
+int espacosVazios(char matriz[m][m]) {
+	int cont=0, i, j;
+	for (i=0; i<m; i++)
+		for (j=0; j<m; j++) 
+			if (matriz[i][j] == ' ')
+				cont++;
+	if (cont != 0)
+		return cont+1;
+	return cont;
+}
+
+void imprimeTabuleiro(char matriz[m][m]) {
+	int i, j;
+	printf("  | ");
+	for (i=0; i<m; i++)
+		printf("%d  ", i);
+	printf("\n");
+	printf("--+-");
+	for (i=0; i<m; i++)
+		printf("---", i);
+	printf("\n");
+	for (i=0; i<m; i++) {
+		printf("%d | ", i);
+		for (j=0; j<m; j++) {
+			printf("%c  ", matriz[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
 
 
