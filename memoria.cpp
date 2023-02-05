6f3945609ec23ca66e8a5917ec3016f5c908e1cd
Carlos Varela <caduvarela@gmail.com>
2023-02-05 15:59:54 -0300

    Dificuldades Funcionando
    Implementando dificuldades funcionais para a porra do jogo.

--- memoria.cpp
+++ memoria.cpp
@@ -4,33 +4,22 @@
 #include <windows.h>
 #include <conio.h>
 #include <locale.h>
-#define m 4 // Largura/altura da matriz quadrada
+//#define tamanho 4 // Largura/altura da matriz quadrada
 #define s 5000 // Tempo (ms) para o usuario memorizar o tabuleiro
 
-/*
-void imprimirMatriz(char matriz[m][m]) {
-	int i, j;
-	for (i=0; i<m; i++) {
-		printf("%d | ", i);
-		for (j=0; j<m; j++) {
-			printf("%c  ", matriz[i][j]);
-		}
-	}
-}
-*/
-
 //Protótipos
-int espacosVazios(char [][m]);
-void imprimeTabuleiro(char [][m]);
-void jogar();
-void dificuldade();
-
+int espacosVazios(char **, int);
+void imprimeTabuleiro(char **, int);
+void jogar(int);
+int dificuldade();
+char **criarMatriz(int);
 
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 	int continuar = 0, escolha;
+	int tamanho;
 	
-	do{
+	do {
 		system("cls");
 		printf("Seja bem vindo ao jogo da memória!\n");
 		printf("\n 1 - Jogar");
@@ -38,13 +27,17 @@ int main() {
 		printf("\n 0 - Sair");
 		
 		printf("\n\nDigite a opção que desejar: ");
+		
 		escolha = getche();
 		escolha -= 48;
 		fflush(stdin);
 		
 		switch (escolha) {
 			case 1:
-				jogar();
+				//Pergunta ao usuário a dificuldade
+				tamanho = dificuldade();
+				if (tamanho != 0)
+					jogar(tamanho);
 				break;
 			/*case 2:
 				ranking();
@@ -59,46 +52,46 @@ int main() {
 				Sleep(500);
 				break;
 		}
-	}while(!continuar);
+	} while(!continuar);
 
 	system("pause");
 	return 0;
 }
 
-void jogar(){
+void jogar(int tamanho) {
 	
 	system("cls");
 	
-	char tabuleiro[m][m], tabuleiroJogo[m][m];
-	int numCartas = m*m/2;
+	char **tabuleiro, **tabuleiroJogo;
+	int numCartas = tamanho*tamanho/2;
 	char cartas[numCartas];
 	int cont=0, i, j, k;
 	int linha[2], coluna[2];
 	int invalido=0;
 	int score = 0;
 	
-	//Pergunta ao usuário a dificuldade
-	dificuldade();
+	tabuleiro = criarMatriz(tamanho);
+	tabuleiroJogo = criarMatriz(tamanho);
 	
 	// Inicializando cartas
 	for (i=0; i<numCartas; i++)
 		cartas[i] = 65 + i; // 65 e o numero na tabela ascii onde comeca o alfabeto maiusculo
 	
 	// Inicializando o tabuleiro e tabuleiroJogo
-	for (i=0; i<m; i++) 
-		for (j=0; j<m; j++) {
+	for (i=0; i<tamanho; i++) 
+		for (j=0; j<tamanho; j++) {
 			tabuleiro[i][j] = ' ';
 			tabuleiroJogo[i][j] = '*';
 		}
 	
 	// Monta o tabuleiro
 	srand(time(NULL));
-	while (espacosVazios(tabuleiro) > 0) {
+	while (espacosVazios(tabuleiro, tamanho) > 0) {
 		for (k=0; k<numCartas; k++) {
 			cont=0;
 			while (cont<2) {
-				i=rand()%m;
-				j=rand()%m;
+				i=rand()%tamanho;
+				j=rand()%tamanho;
 				if (tabuleiro[i][j] == ' ') {
 					tabuleiro[i][j] = cartas[k];
 					cont++;
@@ -109,20 +102,20 @@ void jogar(){
 	
 	// Mostra o tabuleiro
 	printf("Memorize o tabuleiro...\n");
-	imprimeTabuleiro(tabuleiro);
+	imprimeTabuleiro(tabuleiro, tamanho);
 	
 	// Informa o tempo até que o jogo comece
 	printf("\nIniciando jogo em:");
 	for (i=s/1000; i>=0; i--) {
 		printf(" %d", i);
-		Sleep(s/(s/1000));
+		Sleep(1000);
 	}
 	
 	// Inicia Jogo
 	do {
 		for (i=0; i<2; i++) {
 			system("cls");
-			imprimeTabuleiro(tabuleiroJogo);
+			imprimeTabuleiro(tabuleiroJogo, tamanho);
 			linha[i] = -1;
 			coluna[i] = -1;
 			do {
@@ -133,8 +126,11 @@ void jogar(){
 				fflush(stdin);
 				
 				// Verifica se o valor de linha e coluna estao dentro do intervalo possivel
-				if ((linha[i] < 0 || linha[i] >= m || coluna[i] < 0 || coluna[i] >= m)) {
+				if ((linha[i] < 0 || linha[i] >= tamanho || coluna[i] < 0 || coluna[i] >= tamanho)) {
 					printf("Posicao invalida!");
+					Sleep(800);
+					system("cls");
+					imprimeTabuleiro(tabuleiroJogo, tamanho);
 					invalido = 1;
 					continue;
 				}
@@ -144,12 +140,15 @@ void jogar(){
 					tabuleiroJogo[linha[i]][coluna[i]] = tabuleiro[linha[i]][coluna[i]];
 				else {
 					printf("A posicao ja foi revelada!");
+					Sleep(800);
+					system("cls");
+					imprimeTabuleiro(tabuleiroJogo, tamanho);
 					invalido = 1;
 				}
 			} while (invalido == 1);
 		}
 		system("cls");
-		imprimeTabuleiro(tabuleiroJogo);
+		imprimeTabuleiro(tabuleiroJogo, tamanho);
 		if (tabuleiroJogo[linha[0]][coluna[0]] != tabuleiroJogo[linha[1]][coluna[1]]) {
 			printf("\nNope");
 			Sleep(800);
@@ -162,51 +161,49 @@ void jogar(){
 	} while (linha[i]+coluna[i]!=6);
 }
 
-void dificuldade(){
+int dificuldade() {
 	int continuar = 0;
 	int dificuldade;
 	
 	do{
+		system("cls");
 		printf("\n 0 - Voltar ao Menu");
 		printf("\n 1 - Fácil");
 		printf("\n 2 - Médio");
 		printf("\n 3 - Difícil");
 		printf("\n\nEscolha a dificuldade: ");
-		scanf("%d",&dificuldade);
 		
 		dificuldade = getche();
 		dificuldade -= 48;
 		fflush(stdin);
 		
-		switch(dificuldade){
+		continuar = 1;
+		switch(dificuldade)	{
 			case 0:
-				main();
+				return 0;
 				break;
 			case 1:
-				#define m 4
-				continuar = 1;
+				return 4;
 				break;
 			case 2:
-				#define m 5
-				continuar = 1;
+				return 5;
 				break;
 			case 3:
-				#define m 6
-				continuar = 1;
+				return 6;
 				break;
 			default:
+				continuar = 0;
 				printf("Opção inválida!\n");
 				Sleep(500);
 				break;
-			
 		}	
-	}while(!continuar);
+	} while(!continuar);
 }
 
-int espacosVazios(char matriz[m][m]) {
+int espacosVazios(char **matriz, int tamanho) {
 	int cont=0, i, j;
-	for (i=0; i<m; i++)
-		for (j=0; j<m; j++) 
+	for (i=0; i<tamanho; i++)
+		for (j=0; j<tamanho; j++) 
 			if (matriz[i][j] == ' ')
 				cont++;
 	if (cont != 0)
@@ -214,25 +211,38 @@ int espacosVazios(char matriz[m][m]) {
 	return cont;
 }
 
-void imprimeTabuleiro(char matriz[m][m]) {
+void imprimeTabuleiro(char **matriz, int tamanho) {
 	int i, j;
 	printf("  | ");
-	for (i=0; i<m; i++)
+	for (i=0; i<tamanho; i++)
 		printf("%d  ", i);
 	printf("\n");
 	printf("--+-");
-	for (i=0; i<m; i++)
+	for (i=0; i<tamanho; i++)
 		printf("---", i);
 	printf("\n");
-	for (i=0; i<m; i++) {
+	for (i=0; i<tamanho; i++) {
 		printf("%d | ", i);
-		for (j=0; j<m; j++) {
+		for (j=0; j<tamanho; j++) {
 			printf("%c  ", matriz[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-
-
-
+char **criarMatriz(int tamanho) {
+	char **matriz;
+	int i;
+	
+	for (i=0; i<tamanho; i++) {
+		matriz = (char **) calloc(tamanho, tamanho*sizeof(char));
+		if (matriz == NULL) return NULL;
+	}
+		
+	for (i=0; i<tamanho; i++) {
+		matriz[i] = (char *) calloc(tamanho, tamanho*sizeof(char));
+		if (matriz[i] == NULL) return NULL;
+	}
+		
+	return matriz;
+}
